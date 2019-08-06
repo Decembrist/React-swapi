577c173c22afd46f17498f2c77438528e46c7055
andrew <decembrist86@ya.ru>
2019-08-06 11:53:20 +0300

    Добавлена функция трансформации объекта и произведен рефакторинг

--- src/servises/swapi-service.js
+++ src/servises/swapi-service.js
@@ -1,41 +1,84 @@
 export default class SwapiService {
 
-  _apiBase = 'https://swapi.co/api';
+    _apiBase = 'https://swapi.co/api';
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
 
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}` +
-        `, received ${res.status}`)
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` +
+                `, received ${res.status}`)
+        }
+        return await res.json();
     }
-    return await res.json();
-  }
 
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results.map(this._transformPerson);
+    }
 
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`);
-  }
+    async getPerson(id) {
+        const person = await this.getResource(`/people/${id}/`);
+        return this._transformPerson(person)
+    }
 
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
+    _transformPerson(person) {
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birthYear,
+            eyeColor: person.eyeColor
+        }
+    }
 
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
-  }
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`);
+        return res.results.map(this._transformPlanet);
+    }
 
-  async getAllStarships() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
+    async getPlanet(id) {
+        const planet = await this.getResource(`/planets/${id}/`);
+        return this._transformPlanet(planet);
+    }
 
-  getStarship(id) {
-    return this.getResource(`/starships/${id}/`);
-  }
+    _extractId(planet) {
+        const idRegExp = /\/([0-9]*)\/$/;
+
+        return planet.url.match(idRegExp)[1];
+    }
+
+    _transformPlanet(planet) {
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotation: planet.rotation_period,
+            diameter: planet.diameter
+        };
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.results.map(this._transformStarship);
+    }
+
+    async getStarship(id) {
+        const starship = await this.getResource(`/starships/${id}/`);
+        return this._transformStarship(starship);
+    }
+
+    _transformStarship(starship) {
+        return {
+            id: this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.costInCredits,
+            length: starship.length,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargoCapacity
+        }
+    }
 }
